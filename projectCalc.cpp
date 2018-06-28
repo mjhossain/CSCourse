90331511c1a20317a4be74be21e31e110b8a7ed4
zakir <mjhossainnyc@gmail.com>
2018-06-27 21:11:11 -0400

    Classwork update

--- projectCalc.cpp
+++ projectCalc.cpp
@@ -47,3 +47,40 @@ void classWork()
 
 }
 */
+
+/* Temperature Calculator
+
+
+    double celsius;
+    double farenheit;
+    double result;
+    char choice;
+
+
+    cout << "Welcome to Temperature Converter\n\n"
+    << "Enter 1 for C to F\n"
+    << "Enter 2 for F to C\n\n"
+    << "Enter Your Choice: ";
+    cin >> choice;
+
+
+    switch(choice) {
+    case '1':
+        cout << "\n\nEnter Temperature in C: ";
+        cin >> celsius;
+        result = (celsius * 1.8) + 32;
+        cout << result << "\n\n";
+        break;
+    case '2':
+        cout << "\n\nEnter Fahrenheit in F: ";
+        cin >> farenheit;
+        result = (farenheit - 32) * 0.5556;
+        cout << result << "\n\n";
+        break;
+    default:
+        cout << "Invalid Choice\n\n";
+    }
+
+    */
+
+
